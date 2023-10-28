a4672858673e687c6bcfeabf391248f80acfd9be
sujikim <sujikim@student.42seoul.kr>
2023-10-28 18:08:53 +0900

    fix: 리스트 카드 css 수정

--- src/components/pokemon/Card.tsx
+++ src/components/pokemon/Card.tsx
@@ -1,5 +1,6 @@
 import Text from 'components/Text'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import { Pokemon } from 'types/Pokemon'
 
 type Props = {
@@ -7,11 +8,30 @@ type Props = {
 }
 const Card = ({ pokemon }: Props) => {
   return (
-    <Link to={`/detail/${pokemon.id}`}>
-      <Text>{`#${pokemon.id}`}</Text>
-      <Text>{pokemon.name}</Text>
-    </Link>
+    <CardWrapper>
+      <CardLink to={`/detail/${pokemon.id}`}>
+        <Text type="subtitle">{`#${pokemon.id}`}</Text>
+        <Text>{pokemon.name}</Text>
+      </CardLink>
+    </CardWrapper>
   )
 }
 
 export default Card
+
+const CardWrapper = styled.li`
+  list-style: none;
+  width: 20rem;
+  border: 1px solid black;
+  border-radius: 0.5rem;
+`
+const CardLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 10rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: inherit;
+`
--- src/components/pokemon/List.tsx
+++ src/components/pokemon/List.tsx
@@ -1,16 +1,27 @@
 import { Pokemon } from 'types/Pokemon'
+import Card from './Card'
+import styled from 'styled-components'
 
 type Props = {
   pokemons: Pokemon[]
 }
 const PokemonList = ({ pokemons }: Props) => {
   return (
-    <ul>
+    <ListWrapper>
       {pokemons.map((pokemon) => (
-        <li key={pokemon.id}>{pokemon.name}</li>
+        <Card pokemon={pokemon} key={pokemon.id} />
       ))}
-    </ul>
+    </ListWrapper>
   )
 }
 
 export default PokemonList
+
+const ListWrapper = styled.ol`
+  display: flex;
+  flex-wrap: wrap;
+  width: 90%;
+  justify-content: center;
+  margin: 4rem auto;
+  gap: 1rem;
+`
