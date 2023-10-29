74f8de12699ec8239aaa3e28cc7a5c9ee656de71
sujikim <sujikim@student.42seoul.kr>
2023-10-30 04:48:37 +0900

    fix: 새로고침 후 페이지 이동시 suspence 오류 수정
    Layout 화면에 suspence 추가해서 해결

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,13 +1,16 @@
-import Header from 'components/Header'
+import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import styled from 'styled-components'
+import Header from 'components/Header'
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Main>
-        <Outlet />
+        <Suspense>
+          <Outlet />
+        </Suspense>
       </Main>
     </>
   )
