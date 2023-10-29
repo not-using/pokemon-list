41890f3d7349cac64fd491eaf6aef2b157a3cd81
sujikim <sujikim@student.42seoul.kr>
2023-10-30 01:33:27 +0900

    fix: 검색창 input 태그에 숫자만 가능하도록 수정 및 에러메세지 삭제
    불필요한 상태 대신 native tag 기능 활용

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,24 @@
 import { FormEventHandler, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-import Text from './Text'
 
 const SearchBar = () => {
   const [searchedValue, setSearchedValue] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
   const navigate = useNavigate()
 
   const goToPokemonDetail: FormEventHandler = (e) => {
     e.preventDefault()
-    if (isNaN(Number(searchedValue))) {
-      setErrorMessage('숫자만 입력해주세요')
-      return
-    }
     navigate(`/${searchedValue}`)
-    setErrorMessage('')
   }
   
   return (
     <SearchBarWrapper onSubmit={goToPokemonDetail}>
       <input
-        type="text"
+        type="number"
         onChange={(e) => setSearchedValue(e.currentTarget.value)}
         placeholder="포켓몬 번호를 입력해주세요"
       />
       <button type="submit">검색</button>
-      <Text>{errorMessage}</Text>
     </SearchBarWrapper>
   )
 }
