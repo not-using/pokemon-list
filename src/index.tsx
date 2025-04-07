0cb9fc63911174ca538c3fe38c995899a7caf1dc
sujikim <sujikim@student.42seoul.kr>
2025-04-07 20:14:35 +0900

    build: ts 최신화 및 CRA 대신 pnpm + vite 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { RecoilRoot } from 'recoil'
+import { Provider } from 'jotai'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <RecoilRoot>
+  <Provider>
     <App />
-  </RecoilRoot>,
+  </Provider>,
 )
