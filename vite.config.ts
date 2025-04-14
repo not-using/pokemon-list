da7c2b4ceaf4474f43024f7d64ff5becf99816d1
sujikim <sujikim@student.42seoul.kr>
2025-04-14 22:19:16 +0900

    build: tailwind 라이브러리 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
   build: { outDir: 'build' },
 })
